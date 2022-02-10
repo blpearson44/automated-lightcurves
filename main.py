88341a016158fefa88d2f1f387b32e10c6b9e275
Ben Pearson <blpearson44@icloud.com>
2022-02-10 12:46:39 -0800

    added plotting and fixed csv export

--- main.py
+++ main.py
@@ -2,20 +2,27 @@
 
 # Imports
 import typer
+import math
+import os
 import pandas as pd
 from datetime import date
 from astropy.io import fits
+import matplotlib.pyplot as plt
 import photometryplus.photometry as pho
 
 
-# star_RA = 323.4318115915129166
-# star_DEC = 51.123536172913055
-# # input_file = "/Users/ben/projects/Senior-Thesis/test.fts"
-# input_file = "/Users/ben/projects/Senior-Thesis/RX_J2133.7+5107 (V)/RX_J2133.7+5107 V-20200505at082302_-25-1X1-300-V.fts"
-# dark = "/Users/ben/projects/Senior-Thesis/calibrations/darks/master_dark_20211007_1X1_300.fits"
-# flats = "/Users/ben/projects/Senior-Thesis/calibrations/flats/master_flat_20210722_1X1_V.fits"
+class NoFileFoundError(Exception):
+    """Throw this error when a file cannot be found."""
 
-app = typer.Typer()
+    pass
+
+
+CALIBRATION_PATH = "/Users/ben/Projects/Senior-Thesis/calibrations/"
+
+
+def is_non_zero_file(path: str) -> bool:
+    """Returns false if file is empty or does not exist, true otherwise."""
+    return os.path.isfile(path) and os.path.getsize(path) > 0
 
 
 def find_dark(input_file: str) -> str:
@@ -32,16 +39,34 @@ def find_dark(input_file: str) -> str:
         └──flats/
             └──flats fits data
     """
-    dark = ""
+    path = CALIBRATION_PATH + "darks/"  # default path
     dark_date = 0
     dark_exposure = 0
     input_date = 0
     input_exposure = 0
     with fits.open(input_file) as hdul:
-        input_date = hdul[0].header["JD"]  # TODO cal type
+        # input_date = hdul[0].header["JD"]  # TODO check allowed tolerances with date
+        #
         input_exposure = hdul[0].header["EXPOSURE"]
 
-    return dark
+    for dark_file in os.listdir(path):
+        if dark_file.endswith(".fits") or dark_file.endswith(".fts"):
+            full_path = path + dark_file
+            with fits.open(full_path) as hdul:
+                # dark_date = hdul[0].header["JD"]
+                dark_exposure = hdul[0].header["EXPOSURE"]
+                if (
+                    hdul[0].header["IMAGETYP"] == "Dark Frame"
+                    # and math.isclose(dark_date, input_date, abs_tol=20)
+                    and math.isclose(
+                        dark_exposure, input_exposure, abs_tol=20
+                    )  # TODO check allowed tolerances with exposure time
+                ):
+                    return full_path
+        else:
+            pass
+
+    raise NoFileFoundError("No dark file found.")
 
 
 def find_flat(input_file: str) -> str:
@@ -58,13 +83,40 @@ def find_flat(input_file: str) -> str:
         └──flats/
             └──flats fits data
     """
-    flat = ""
-    return flat
+    path = CALIBRATION_PATH + "flats/"  # default path
+    flat_date = 0
+    flat_exposure = 0
+    input_date = 0
+    input_exposure = 0
+    with fits.open(input_file) as hdul:
+        input_date = hdul[0].header["JD"]
+        # TODO Check filter type
+        # input_exposure = hdul[0].header["EXPOSURE"]
+
+    for flat_file in os.listdir(path):
+        if flat_file.endswith(".fits") or flat_file.endswith(".fts"):
+            full_path = path + flat_file
+            with fits.open(full_path) as hdul:
+                # flat_date = hdul[0].header["JD"]
+                flat_exposure = hdul[0].header["EXPOSURE"]
+                if (
+                    hdul[0].header["IMAGETYP"]
+                    == "Flat Field"
+                    # and math.isclose(flat_date, input_date, abs_tol=20)  # TODO check allowed tolerances on date for flat field
+                    # and math.isclose(flat_exposure, input_exposure, abs_tol=20)  # TODO double check that exposure is not needed
+                ):
+                    return full_path
+        else:
+            pass
+
+    raise NoFileFoundError("No flat file found.")
 
 
-# TODO Manually input size of pixels
+# TODO Manually input size of aperture
 # TODO Manually input reference stars
 # TODO Option for pre-calibrated files
+
+app = typer.Typer()
 
 
 @app.command()
@@ -77,32 +129,58 @@ def run_photometry(
     save: bool = False,
     output_file: str = "./Output/output.csv",
 ) -> None:
-    """Run photometry on target star."""
+    """
+    Run photometry on target star.
+    """
     if dark is None:
-        find_dark(input_file)
+        try:
+            dark = find_dark(input_file)
+        except NoFileFoundError:
+            print("No dark file found.")
+            return
     if flat is None:
-        find_flat(input_file)
+        try:
+            flat = find_flat(input_file)
+        except NoFileFoundError:
+            print("No flat file found.")
+            return
+
     output = pho.runPhotometry(star_ra, star_dec, input_file, dark, "", flat)
+    date_taken = ""
+    with fits.open(input_file) as hdul:
+        date_taken = hdul[0].header["JD"]
+
     pho.printReferenceToFile(
-        output.referenceStars, "./reference-stars/" +
-        str(date.today()) + ".csv"
+        output.referenceStars, "./reference-stars/" + str(round(date_taken)) + ".csv"
     )
     if save:
-        date_taken = ""
-        with fits.open(input_file) as hdul:
-            date_taken = hdul[0].header["JD"]
         df = pd.DataFrame(
             {
-                "Date Added": [date.today()],
                 "Date Taken": [date_taken],
-                "magnitude": [output.magnitude],
-                "error": [output.error],
+                "Magnitude": [output.magnitude],
+                "Error": [output.error],
             }
         )
-        df.to_csv(output_file, mode="a", index=False, header=False)
+        if is_non_zero_file(output_file):
+            df.to_csv(output_file, mode="a", index=False, header=False)
+        else:
+            df.to_csv(output_file, mode="w", index=False, header=True)
 
 
-pho.changeSettings(useBiasFlag=0)
+pho.changeSettings(useBiasFlag=0, consolePrintFlag=0)
+
+
+@app.command()
+def plot_lightcurve(
+    input_file: str = "./Output/output.csv", output_file: str = "./Output/plot.png"
+) -> None:
+    """Plot lightcurve using JD dates, magnitude, and errors."""
+    df = pd.read_csv(input_file)
+    plt.scatter(df["Date Taken"], df["Magnitude"])
+    plt.errorbar(df["Date Taken"], df["Magnitude"], yerr=df["Error"], fmt="o")
+    plt.savefig(output_file)
+    return
+
 
 if __name__ == "__main__":
     app()
