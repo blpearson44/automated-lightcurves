dc52d5b20ed44fe5c11204c81caa41469fa732c9
Ben Pearson <blpearson44@icloud.com>
2022-02-26 13:34:34 -0800

    added better documentation

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class NoFileFoundError(Exception):
     """Throw this error when a file cannot be found."""
 
 
-CALIBRATION_PATH = "/Users/ben/Projects/Senior-Thesis/calibrations/"
+CALIBRATION_PATH = "./calibrations/"
 
 app = typer.Typer()
 
@@ -97,15 +97,22 @@ def find_flat(input_file: str) -> str:
 # TODO Manually input size of aperture
 # TODO Manually input reference stars
 # TODO Option for pre-calibrated files
-# TODO Secondary date axis
-# TODO Use error bars to determine if data points should be kept
 
 
 @app.command()
 def index_dir(path: str) -> None:
-    """Index the fits files in a directory."""
+    """
+    Index the fits files in a directory.
+    Gathers:
+    MJD
+    ImageType (Light, Dark Frame, Flat Field)
+    Exposure length
+    File Path
+    Presence of WCS (True/False)
+    Filter type
+    """
     index = {
-        "JD": [],
+        "MJD": [],
         "IMAGETYP": [],
         "EXPOSURE": [],
         "FILEPATH": [],
@@ -115,7 +122,7 @@ def index_dir(path: str) -> None:
     for fits_file in os.scandir(path):
         if fits_file.path.endswith(".fits") or fits_file.path.endswith(".fts"):
             with fits.open(fits_file.path) as hdul:
-                index["JD"].append(hdul[0].header["JD"])
+                index["MJD"].append(hdul[0].header["JD"]) - 2400000.5
                 index["IMAGETYP"].append(hdul[0].header["IMAGETYP"])
                 index["EXPOSURE"].append(hdul[0].header["EXPOSURE"])
                 index["WCS"].append("CD1_1" in hdul[0].header)
@@ -142,6 +149,9 @@ def run_photometry(
 ) -> None:
     """
     Run photometry on target star.
+
+    Optionally manually set calibration files with --dark and --flat.
+    Save data with --save and set output file with --output-file.
     """
     # Find calibration files if none are provided
     if dark is None:
@@ -173,7 +183,7 @@ def run_photometry(
     if save:
         df = pd.DataFrame(
             {
-                "Date Taken": [date_taken],
+                "MJD": [date_taken - 2400000.5],
                 "Magnitude": [output.magnitude],
                 "Error": [output.error],
             }
@@ -190,11 +200,26 @@ def run_photometry_bulk(
     star_dec: float,
     input_dir: str,
 ) -> None:
-    """Run photometry on a directory."""
+    """
+    Run photometry on a directory. Automatically sources calibration files assuming they will be found in
+    CALIBRATION_PATH/darks/dark_fits_data
+    CALIBRATION_PATH/flats/flat_fits_data
+    CALIBRATION_PATH is set in main.py and defaults to ./calibrations/
+    """
     for input_file in os.listdir(input_dir):
         if input_file.endswith(".fits") or input_file.endswith(".fts"):
             full_path = input_dir + input_file
             run_photometry(star_ra, star_dec, full_path, save=True)
+
+
+# conversion from MJD to date
+def mjdtodt(mjd):
+    """Return a floating point UNIX timestamp for a given MJD"""
+    return [date.fromtimestamp((m - 40587) * 86400.0) for m in mjd]
+
+
+def dttomjd(dttime):
+    return dttime
 
 
 @app.command()
@@ -203,18 +228,25 @@ def plot_lightcurve(
     output_file: str = "./Output/plot.png",
     title: str = "Light Curve",
 ) -> None:
-    """Plot lightcurve using JD dates, magnitude, and errors."""
+    """
+    Plot lightcurve using JD dates, magnitude, and errors.
+    """
     df = pd.read_csv(input_file)
     for i in range(len(df["Error"])):
         if abs(df["Error"][i]) > 0.1:
             df = df.drop(i)
-    plt.scatter(df["Date Taken"], df["Magnitude"])
+    f = plt.figure()
+    f.set_figwidth(25)
+    f.set_figheight(5)
+    plt.scatter(df["MJD"], df["Magnitude"])
     ax = plt.gca()
     ax.invert_yaxis()
-    ax.set_xlabel("Date Taken")
+    ax.set_xlabel("MJD")
     ax.set_ylabel("Magnitude")
-    plt.errorbar(df["Date Taken"], df["Magnitude"], yerr=df["Error"], fmt="o")
+    ax.secondary_xaxis("top", functions=(mjdtodt, dttomjd))
+    plt.errorbar(df["MJD"], df["Magnitude"], yerr=df["Error"], fmt="o")
     plt.title(title, fontsize=15, fontweight="bold")
+    plt.tight_layout()
     plt.savefig(output_file)
 
 
