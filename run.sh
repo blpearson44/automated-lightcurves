e74f3f666acc4a86f18499260838d8042864d5df
Ben Pearson <blpearson44@icloud.com>
2022-03-29 09:17:27 -0700

    okaay this time

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ log="./Output/logs/log_$now.txt"
 dropbox="/home/bpearson/Dropbox/IP-monitoring-output/"
 touch $log
 /opt/local/anaconda3/bin/python ./run_wcs.py &>> $log
-cp -r ./Output/logs/ $dropbox/logs/
+cp -r ./Output/logs/ $dropbox/
 cp ./Output/*.csv $dropbox/data/
 cp ./Output/*.png $dropbox/plots/
-cp -r ./Output/indexes/ $dropbox/indexes/
+cp -r ./Output/indexes/ $dropbox/
